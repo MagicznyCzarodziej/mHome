aeafdebc1c81d75b121a60fbe2d39641468ed569
Przemysław Pitus <nexit07@gmail.com>
2021-11-23 22:50:57 +0100

    Add desktop styles to group view

--- Client/src/components/layouts/GroupLayout/GroupLayout.tsx
+++ Client/src/components/layouts/GroupLayout/GroupLayout.tsx
@@ -3,13 +3,26 @@ import React from 'react';
 import styles from './GroupLayout.module.sass';
 import { GroupTopBar } from 'components/GroupTopBar/GroupTopBar';
 import { BottomNavigation } from 'components/BottomNavigation/BottomNavigation';
+import { SideBar } from 'components/SideBar/SideBar';
+import { useMediaQuery } from '@react-hook/media-query';
 
 export const GroupLayout: React.FC = (props) => {
-  return (
-    <div className={styles.layout}>
-      <GroupTopBar />
-      <div className={styles['layout__page-content']}>{props.children}</div>
-      <BottomNavigation />
-    </div>
-  );
+  const isDesktop = useMediaQuery('(min-width: 600px)');
+
+  if (isDesktop) {
+    return (
+      <div className={styles['layout--desktop']}>
+        <SideBar />
+        <div className={styles['layout__page-content']}>{props.children}</div>
+      </div>
+    );
+  } else {
+    return (
+      <div className={styles.layout}>
+        <GroupTopBar />
+        <div className={styles['layout__page-content']}>{props.children}</div>
+        <BottomNavigation />
+      </div>
+    );
+  }
 };
